ad577b632ae3ff616205efb7c52cb1f6311e550b
Kristoffer <krigjo25@gmail.com>
2025-05-12 09:22:33 +0200

    IC

--- GetNews.Core/DomainModel/VerificationError.cs
+++ GetNews.Core/DomainModel/VerificationError.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GetNews.Core.DomainModel
-{
-    public enum VerificationError 
-    {
-        SubscriptionNotFound,
-        InvalidCodeFormat,
-        VerificationCodeMissing,
-        CodeMismatch,
-        EmailMismatch
-    }
-
-}
+﻿
