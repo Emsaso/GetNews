376b3785e0721f44ec39da221599a992802749e5
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-05-12 14:20:00 +0200

    lagt til en ny klasse og laget feil håndtering

---
+++ GetNews.API/ApiModel/EmailAddress.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GetNews.API.ApiModel
+{
+    public class EmailAddress
+    {
+        public string Value { get; }
+
+        public EmailAddress(string value)
+        {
+            Value = value;
+        }
+
+        public bool IsValid()
+        {
+            return Value.Contains('@') && Value.Contains('.');
+        }
+        public bool IsEqual(string emailAddress)
+        {
+            return string.Equals(Value, emailAddress.Trim().ToLower(), StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
