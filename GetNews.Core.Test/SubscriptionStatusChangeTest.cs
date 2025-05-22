38dd3181cb53aa9a584a1ac1536ce5284c5a74f9
GET Academy <terje@kolderup.net>
2025-05-22 10:50:37 +0200

    x

---
+++ GetNews.Core.Test/SubscriptionStatusChangeTest.cs
@@ -0,0 +1,50 @@
+﻿using GetNews.Core.ApplicationService;
+using GetNews.Core.DomainModel;
+
+namespace GetNews.Core.Test
+{
+    internal class SubscriptionStatusChangeTest
+    {
+
+        [Test]
+        public void TestSignUpAlreadySubscribed()
+        {
+            var emailAddress = "no-replay@getAcademy.no";
+            var subscription = new Subscription(emailAddress, SubscriptionStatus.Verified, null, true);
+
+            var result = SubscriptionService.SignUp(emailAddress, subscription);
+
+            Assert.That(result.IsSuccess, Is.False);
+            Assert.That(result.Error, Is.EqualTo(nameof(SignUpError.AlreadySubscribed)));
+        }
+
+        [Test]
+        public void TestSignUpUnsubscribed()
+        {
+            var emailAddress = "no-replay@getAcademy.no";
+            var subscription = new Subscription(emailAddress, SubscriptionStatus.Unsubscribed);
+
+            var result = SubscriptionService.SignUp(emailAddress, subscription);
+
+            Assert.That(result.IsSuccess, Is.True);
+            Assert.That(result.Value.Email, Is.InstanceOf<Email>());
+            Assert.That(result.Value.Subscription, Is.EqualTo(subscription));
+            Assert.That(subscription.Status, Is.EqualTo(SubscriptionStatus.SignedUp));
+        }
+
+        [Test]
+        public void TestSignUpWhenSignedUp()
+        {
+            var emailAddress = "no-replay@getAcademy.no";
+            var subscription = new Subscription(emailAddress, SubscriptionStatus.SignedUp);
+
+            var result = SubscriptionService.SignUp(emailAddress, subscription);
+
+            Assert.That(result.IsSuccess, Is.True);
+            Assert.That(result.Value.Email, Is.InstanceOf<Email>());
+            Assert.That(result.Value.Subscription, Is.EqualTo(subscription));
+            Assert.That(subscription.Status, Is.EqualTo(SubscriptionStatus.SignedUp));
+        }
+
+    }
+}
--- GetNews.Core/ApplicationService/SubscriptionService.cs
+++ GetNews.Core/ApplicationService/SubscriptionService.cs
@@ -7,60 +7,23 @@ namespace GetNews.Core.ApplicationService
 
         public static Result<EmailAndSubscription> SignUp(string emailAddressStr, Subscription? subscription)
         {
-          /*  var emailAddress = new EmailAddress(emailAddressStr);
-            if (subscription != null && subscription.Status == SubscriptionStatus.SignedUp)
-                return Result<EmailAndSubscription>.Fail(SignUpError.AlreadySignedUp);
-
-
-
-            var isUnsubscribed = subscription.Status == SubscriptionStatus.Unsubscribed;
-            
-            else if(subscription.Status == SubscriptionStatus.Unsubscribed) 
-                subscription.ChangeStatus(SubscriptionStatus.SignedUp);
-
-            var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode);
-            return Result<EmailAndSubscription>.Ok(new EmailAndSubscription(mail, subscription));
-          */
-
             var emailAddress = new EmailAddress(emailAddressStr);
-            //var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode); //Testen Feiler når variablen ligger her
-
             if (!emailAddress.IsValid())
                 return Result<EmailAndSubscription>.Fail(SignUpError.InvalidEmailAddress);
-            if (subscription == null) subscription = new Subscription(emailAddressStr);
+            if (subscription?.Status == SubscriptionStatus.Verified)
+                return Result<EmailAndSubscription>.Fail(SignUpError.AlreadySubscribed);
+            subscription ??= new Subscription(emailAddressStr);
 
             var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode);
 
-            switch (subscription.Status)
-                {
-                /*
-                case SubscriptionStatus.SignedUp or SubscriptionStatus.Unsubscribed:
-                    var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode);
-                    if (subscription.Status == SubscriptionStatus.SignedUp) 
-                    {
-                        subscription.ChangeStatus(SubscriptionStatus.SignedUp);
-                        subscription.RegenerateVerificationCode();
-                        return Result<EmailAndSubscription>.Ok(new EmailAndSubscription(mail, subscription));
-                    }
-                    return Result<EmailAndSubscription>.Ok(new EmailAndSubscription(mail, subscription));
-                 */
+            if (subscription.Status == SubscriptionStatus.Unsubscribed)
+            {
+                subscription.ChangeStatus(SubscriptionStatus.SignedUp);
+                subscription.RegenerateVerificationCode();
+            }
 
-                case SubscriptionStatus.SignedUp:
-                    //var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode);
-                    return Result<EmailAndSubscription>.Ok(new EmailAndSubscription(mail, subscription));
-
-                case SubscriptionStatus.Verified:
-                    return Result<EmailAndSubscription>.Fail(SignUpError.AlreadySubscribed);
-
-                case SubscriptionStatus.Unsubscribed:
-                    subscription.ChangeStatus(SubscriptionStatus.SignedUp);
-                    subscription.RegenerateVerificationCode();
-                    //var mail = Email.CreateConfirmEmail(emailAddressStr, subscription.VerificationCode);
-                    return Result<EmailAndSubscription>.Ok(new EmailAndSubscription(mail, subscription));
-                    
-                default:
-                    return Result<EmailAndSubscription>.Fail(SignUpError.Unknown);
-                }
+            var emailAndSubscription = new EmailAndSubscription(mail, subscription);
+            return Result<EmailAndSubscription>.Ok(emailAndSubscription);
         }
 
         public static Result<Subscription> Confirm(string userMail, Guid verificationCode, Subscription subscription)
